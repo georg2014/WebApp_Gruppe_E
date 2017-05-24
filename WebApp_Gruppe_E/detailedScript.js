5aa4a3ea46080efd5a1b9d8cfa7248dcdf9e8618
SirFailaL0t <jakob-feldmann@outlook.com>
2017-05-24 20:30:05 +0200

    nearly finished

---
+++ WebApp_Gruppe_E/detailedScript.js
@@ -0,0 +1,60 @@
+
+$( document ).ready(function() {
+
+	var maxStromstärke = parseInt($("#maxA").text());
+	var gerätekennung = $("#gk").text();
+
+	var spannung = rand(220,240);
+	var stromstärke = rand(0,120);
+
+	$("#spannung").text(spannung);
+	$("#stromstärke").text(stromstärke);
+
+	if(stromstärke>maxStromstärke+5){
+		alert("Die maximale Stromstärke wird überschritten!");
+	}
+
+});
+
+function addrow(nk, zs){
+	var table = document.getElementById("ablesungen");
+
+	var row = table.insertRow(1);
+
+	var zelle1 = row.insertCell(0);
+	var zelle2 = row.insertCell(1);
+	var zelle3 = row.insertCell(2);
+
+	var datum = new Date();
+
+	zelle1.innerHTML = nk;
+	zelle2.innerHTML = zs;
+	zelle3.innerHTML = datum;
+
+}
+
+	var maxRows = 10;
+	var rows = 0;
+function submiter(){
+	if(rows < maxRows){
+	rows += 1;
+	var nutzerkennung = document.forms["ablesen"]["id"].value;
+	var zählerstand = document.forms["ablesen"]["data"].value + " kWh";
+
+	if (nutzerkennung == "") {
+		alert("Geben Sie bitte eine Nutzerkennung ein");
+		return false;
+	}
+	if (zählerstand == "") {
+		alert("Geben Sie bitte einen Zählerstand ein");
+		return false;
+	}
+	else{
+		addrow(nutzerkennung, zählerstand)
+	}
+}
+}
+
+function rand (min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
